166c97c5d10193e1197647f9c3db803cdc7ee991
Avinash Gazula <av530575@dal.ca>
2020-05-29 14:54:38 -0300

    AS-27: Added AES Encryption module

---
+++ src/test/java/com/group8/dalsmartteamwork/EncryptionTest.java
@@ -0,0 +1,29 @@
+package com.group8.dalsmartteamwork;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class EncryptionTest {
+    private static final String TEST_STRING = "HelloWorld123@";
+    private static final String TEST_ENC_STRING = "N3UEE4Uf/MNu9uc3qpeY0g==";
+
+    @Test
+    public void encryptTest() {
+        Encryption encryption = new Encryption();
+        assertTrue(encryption.encrypt(TEST_STRING).equals(TEST_ENC_STRING));
+    }
+
+    @Test
+    public void decryptTest() {
+        Encryption encryption = new Encryption();
+        assertTrue(encryption.decrypt(TEST_ENC_STRING).equals(TEST_STRING));
+    }
+
+    @Test
+    public void encryptDecryptTest() {
+        Encryption encryption = new Encryption();
+        String encrypted_text = encryption.encrypt("HelloWorld123@");
+        assertTrue(encryption.decrypt(encrypted_text).equals("HelloWorld123@"));
+    }
+}
