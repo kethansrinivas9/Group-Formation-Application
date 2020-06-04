390b58abdec94c268d0933f6d56589a6c000e6ae
ramathas <rm291207@dal.ca>
2020-06-04 19:29:34 -0300

    Modified test cases

--- src/test/java/com/group8/dalsmartteamwork/login/loginsecurity/RoleAuthorizationTest.java
+++ src/test/java/com/group8/dalsmartteamwork/login/loginsecurity/RoleAuthorizationTest.java
@@ -1,7 +1,6 @@
 package com.group8.dalsmartteamwork.login.loginsecurity;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
