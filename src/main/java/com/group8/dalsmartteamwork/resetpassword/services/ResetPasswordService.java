e320c2aed3705cec4ba614130b20a1c976f57c7c
ridha <rd428850@dal.ca>
2020-06-17 16:30:50 -0300

    Removed Reset Password Service

--- src/main/java/com/group8/dalsmartteamwork/resetpassword/services/ResetPasswordService.java
+++
@@ -1,7 +0,0 @@
-package com.group8.dalsmartteamwork.resetpassword.services;
-
-import java.sql.SQLException;
-
-public interface ResetPasswordService {
-    Boolean sendPasswordResetMail(String bannerID, String token) throws SQLException ;
-}
