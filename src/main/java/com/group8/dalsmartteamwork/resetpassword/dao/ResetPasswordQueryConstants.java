8f48f47752e9abcdf3cb87e6c9b24ff496beea56
ridha <rd428850@dal.ca>
2020-06-18 03:21:46 -0300

    Changed ResetTokens procedure name

--- src/main/java/com/group8/dalsmartteamwork/resetpassword/dao/ResetPasswordQueryConstants.java
+++ src/main/java/com/group8/dalsmartteamwork/resetpassword/dao/ResetPasswordQueryConstants.java
@@ -2,7 +2,7 @@ package com.group8.dalsmartteamwork.resetpassword.dao;
 
 final class ResetPasswordQueryConstants {
     public static final String INSERT_TOKEN = "INSERT INTO PasswordResetToken (BannerID, Token, Timestamp, Status) VALUES ('%s', '%s', now(), 'valid')";
-    public static final String CALL_UPDATE_TOKEN_STATUS = "CALL resettokens";
+    public static final String CALL_UPDATE_TOKEN_STATUS = "CALL spResetTokens";
     public static final String GET_RESET_REQUEST = "SELECT * FROM PasswordResetToken WHERE BannerID='%s' and token='%s'";
     public static final String UPDATE_PASSWORD = "UPDATE Users SET Password='%s' WHERE BannerID='%s'";
     public static final String UPDATE_REQUEST_STATUS = "UPDATE PasswordResetToken SET Status='expired' WHERE BannerID='%s'";
