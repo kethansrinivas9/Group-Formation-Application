677bb8cb19dd1cfe77205ab4c8fbe9d2f70494cd
ridha <rd428850@dal.ca>
2020-07-04 03:03:03 -0300

    Updated tests to use abstractions

--- src/test/java/com/group8/dalsmartteamwork/resetpassword/NewPasswordTest.java
+++ src/test/java/com/group8/dalsmartteamwork/resetpassword/NewPasswordTest.java
@@ -1,5 +1,6 @@
 package com.group8.dalsmartteamwork.resetpassword;
 
+import com.group8.dalsmartteamwork.resetpassword.models.INewPassword;
 import com.group8.dalsmartteamwork.resetpassword.models.NewPassword;
 import org.junit.jupiter.api.Test;
 
@@ -13,39 +14,39 @@ public class NewPasswordTest {
 
     @Test
     public void defaultConstructorTest() {
-        NewPassword newPassword = new NewPassword();
+        INewPassword newPassword = new NewPassword();
         assertNull(newPassword.getPassword());
     }
 
     @Test
     public void constructorWithTwoArgumentsTest() {
-        NewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
+        INewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
         assertEquals(newPassword.getPassword(), TEMP_PASSWORD, "The password string was not set.");
         assertEquals(newPassword.getBannerID(), TEMP_BANNERID, "The bannerID string was not set.");
     }
 
     @Test
     public void getBannerIDTest() {
-        NewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
+        INewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
         assertEquals(newPassword.getBannerID(), TEMP_BANNERID, "Unable to retrieve password string.");
     }
 
     @Test
     public void setBannerIDTest() {
-        NewPassword newPassword = new NewPassword();
+        INewPassword newPassword = new NewPassword();
         newPassword.setBannerID(TEMP_BANNERID);
         assertEquals(newPassword.getBannerID(), TEMP_BANNERID, "Unable to set BannerID string.");
     }
 
     @Test
     public void getPasswordTest() {
-        NewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
+        INewPassword newPassword = new NewPassword(TEMP_BANNERID, TEMP_PASSWORD);
         assertEquals(newPassword.getPassword(), TEMP_PASSWORD, "Unable to retrieve password string.");
     }
 
     @Test
     public void setPasswordTest() {
-        NewPassword newPassword = new NewPassword();
+        INewPassword newPassword = new NewPassword();
         newPassword.setPassword(TEMP_PASSWORD);
         assertEquals(newPassword.getPassword(), TEMP_PASSWORD, "Unable to set password string.");
     }
--- src/test/java/com/group8/dalsmartteamwork/resetpassword/PasswordPolicyTest.java
+++ src/test/java/com/group8/dalsmartteamwork/resetpassword/PasswordPolicyTest.java
@@ -1,5 +1,6 @@
 package com.group8.dalsmartteamwork.resetpassword;
 
+import com.group8.dalsmartteamwork.resetpassword.models.IPasswordPolicy;
 import com.group8.dalsmartteamwork.resetpassword.models.PasswordPolicy;
 import org.junit.jupiter.api.Test;
 
@@ -12,26 +13,25 @@ import static org.mockito.Mockito.when;
 
 public class PasswordPolicyTest {
 
-    private static final String TEMP_BANNERID = "B00000000";
     private static final String TEMP_PASSWORD = "password123";
 
     @Test
     public void loadPolicyTest() {
-        PasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
+        IPasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
         when(passwordPolicy.loadPolicy()).thenReturn(true);
         assertTrue(passwordPolicy.loadPolicy());
     }
 
     @Test
     public void validateTest() {
-        PasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
+        IPasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
         when(passwordPolicy.isValid(TEMP_PASSWORD)).thenReturn(true);
         assertTrue(passwordPolicy.isValid(TEMP_PASSWORD));
     }
 
     @Test
     public void generateErrorMessageTest() {
-        PasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
+        IPasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
         ArrayList<String> errorMessages = new ArrayList<>();
         errorMessages.add("The password must be atleast 5 characters.");
         errorMessages.add("The password should have atleast 1 special character.");
