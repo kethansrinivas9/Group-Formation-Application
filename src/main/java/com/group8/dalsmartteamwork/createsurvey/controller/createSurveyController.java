852a12263c2ed76c4fc62e828331f3f5cc89fd45
ramathas <rm291207@dal.ca>
2020-07-15 21:07:39 -0300

    created survey page for instructor and TA

---
+++ src/main/java/com/group8/dalsmartteamwork/createsurvey/controller/createSurveyController.java
@@ -0,0 +1,76 @@
+package com.group8.dalsmartteamwork.createsurvey.controller;
+
+import com.group8.dalsmartteamwork.course.Course;
+import com.group8.dalsmartteamwork.createsurvey.dao.CreateSurveyDao;
+import com.group8.dalsmartteamwork.createsurvey.dao.CreateSurveyDaoImpl;
+import com.group8.dalsmartteamwork.createsurvey.model.CreateSurvey;
+import com.group8.dalsmartteamwork.createsurvey.model.CreateSurveyImpl;
+import com.group8.dalsmartteamwork.questions.Question;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class createSurveyController {
+    public Integer courseID;
+    public List<Question> questions = new ArrayList<Question>();
+
+    @GetMapping("/createSurveyPage")
+    public String listCourses(Principal principal, Model model) {
+        CreateSurveyDao createSurveyDao = new CreateSurveyDaoImpl();
+        CreateSurvey createSurvey = new CreateSurveyImpl(createSurveyDao);
+        model.addAttribute(new Course());
+        List<Course> courses = createSurvey.displayListOfCourses(principal.getName());
+        model.addAttribute("list", courses);
+        return "createSurveyPage";
+    }
+
+    @RequestMapping(value = "/createSurveyPage", method = RequestMethod.POST)
+    public String deleteQuestion(@ModelAttribute("course") Course course, Principal principal, Model model,
+            RedirectAttributes redirectAttributes) {
+        CreateSurveyDao createSurveyDao = new CreateSurveyDaoImpl();
+        CreateSurvey createSurvey = new CreateSurveyImpl(createSurveyDao);
+        courseID = course.getCourseID();
+        Boolean status = createSurvey.checkIfSurveyCreated(course.getCourseID());
+        if (status != true) {
+            redirectAttributes.addFlashAttribute("message", "Survey already exists");
+            return "redirect:/questionManager";
+        }
+        return "redirect:/surveyPage";
+    }
+
+    @GetMapping("/surveyPage")
+    public String displaySurveyPage(Principal principal, Model model) {
+        CreateSurveyDao createSurveyDao = new CreateSurveyDaoImpl();
+        CreateSurvey createSurvey = new CreateSurveyImpl(createSurveyDao);
+        Question question = new Question();
+        questions = createSurvey.displayQuestions(principal.getName(), courseID);
+        model.addAttribute("list", questions);
+        model.addAttribute("question", question);
+        return "surveyPage";
+    }
+
+    @RequestMapping(value = "/surveyPage", method = RequestMethod.POST)
+    public String publishSurvey(@RequestParam("question") List<Integer> values, Principal principal, Model model,
+            RedirectAttributes redirectAttributes) {
+        CreateSurveyDao createSurveyDao = new CreateSurveyDaoImpl();
+        CreateSurvey createSurvey = new CreateSurveyImpl(createSurveyDao);
+        Boolean status = createSurvey.addQuestionToSurvey(courseID, values);
+        if (status != true) {
+            redirectAttributes.addFlashAttribute("message", "Survey couldn't be added");
+            return "redirect:/questionManager";
+        }
+        redirectAttributes.addFlashAttribute("message", "Survey published");
+        return "redirect:/questionManager";
+    }
+
+}
---
+++ src/main/java/com/group8/dalsmartteamwork/createsurvey/dao/CreateSurveyDaoImpl.java
@@ -0,0 +1,115 @@
+package com.group8.dalsmartteamwork.createsurvey.dao;
+
+import com.group8.dalsmartteamwork.database.CallStoredProcedure;
+import com.group8.dalsmartteamwork.questions.Question;
+import com.group8.dalsmartteamwork.course.Course;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateSurveyDaoImpl implements CreateSurveyDao {
+    public int published;
+    @Override
+    public List<Course> displayListOfCourses(String BannerID) {
+        List<Course> listOfCourses = new ArrayList<>();
+        CallStoredProcedure procedure = null;
+        ResultSet resultSet;
+        try {
+            procedure = new CallStoredProcedure("spListAllCourses(?)");
+            procedure.setParameter(1, BannerID);
+            resultSet = procedure.executeWithResults();
+            while (resultSet.next()) {
+                int ID = resultSet.getInt(1);
+                listOfCourses.add(new Course(ID));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != procedure) {
+                procedure.cleanup();
+            }
+        }
+        return listOfCourses;
+    }
+
+    @Override
+    public boolean checkIfSurveyCreated(int courseID) {
+        ResultSet resultSet;
+        CallStoredProcedure procedure = null;
+        try {
+            procedure = new CallStoredProcedure("spGetStatus(?)");
+            procedure.setParameter(1, courseID);
+            resultSet = procedure.executeWithResults();
+            while (resultSet.next()) {
+                published = resultSet.getInt(1);
+                if(published > 0){
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != procedure) {
+                procedure.cleanup();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public List<Question> displayQuestions(String BannerID,int courseID) {
+        List<Question> listOfQuestions = new ArrayList<>();
+        CallStoredProcedure procedure = null;
+        ResultSet resultSet;
+        try {
+            procedure = new CallStoredProcedure("spDisplayQuestions(?,?)");
+            
+            procedure.setParameter(1, courseID);
+            procedure.setParameter(2, BannerID);
+            resultSet = procedure.executeWithResults();
+        
+            while (resultSet.next()) {
+                int ID = resultSet.getInt(1);
+                String questionText = resultSet.getString(2);
+                listOfQuestions.add(new Question(ID, questionText));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != procedure) {
+                procedure.cleanup();
+            }
+        }
+        return listOfQuestions;
+    }
+    
+    @Override
+    public boolean addQuestionToSurvey(int courseID,List<Integer> questionID) {
+        CallStoredProcedure procedure = null;
+        int surveyStatus = published+1;
+        try {
+            procedure = new CallStoredProcedure("spInsertQuestions(?, ?)");
+            for(int i =0;i<questionID.size();i++) {
+                procedure.setParameter(1, courseID);
+                procedure.setParameter(2, questionID.get(i));
+                procedure.execute();
+            }
+            procedure = new CallStoredProcedure("spUpdateSurveyStatus(?,?)");
+            procedure.setParameter(1,courseID);
+            procedure.setParameter(2,surveyStatus);
+            procedure.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (null != procedure) {
+                procedure.cleanup();
+            }
+        }
+        
+    }
+
+}
