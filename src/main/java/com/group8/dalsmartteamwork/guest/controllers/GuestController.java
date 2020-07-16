6f958df7071f6e64b1135f92190c8eb94c729348
ridha <rd428850@dal.ca>
2020-07-16 06:34:10 -0300

    Added Logs to Guest Package

--- src/main/java/com/group8/dalsmartteamwork/guest/controllers/GuestController.java
+++ src/main/java/com/group8/dalsmartteamwork/guest/controllers/GuestController.java
@@ -11,14 +11,14 @@ import java.util.List;
 @Controller
 public class GuestController {
 
-    @GetMapping("/viewallcourses")
-    public String viewAllCourszes(Model model) {
-        List<Course> courses;
-        GuestDaoImpl guestDao = new GuestDaoImpl();
-        int courseID = 0;
-        courses = guestDao.getCourses();
-        model.addAttribute("courses", courses);
-        model.addAttribute("courseID", courseID);
-        return "viewCourses";
-    }
+	@GetMapping("/viewallcourses")
+	public String viewAllCourses(Model model) {
+		List<Course> courses;
+		GuestDaoImpl guestDao = new GuestDaoImpl();
+		int courseID = 0;
+		courses = guestDao.getCourses();
+		model.addAttribute("courses", courses);
+		model.addAttribute("courseID", courseID);
+		return "viewCourses";
+	}
 }
