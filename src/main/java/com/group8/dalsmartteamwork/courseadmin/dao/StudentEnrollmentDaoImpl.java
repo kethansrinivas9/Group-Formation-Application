a750ba94c586fc31eaccd648d2343926c9439180
ridha <rd428850@dal.ca>
2020-07-16 06:52:46 -0300

    Added Logs to Course Admin Package

--- src/main/java/com/group8/dalsmartteamwork/courseadmin/dao/StudentEnrollmentDaoImpl.java
+++ src/main/java/com/group8/dalsmartteamwork/courseadmin/dao/StudentEnrollmentDaoImpl.java
@@ -1,25 +1,30 @@
 package com.group8.dalsmartteamwork.courseadmin.dao;
 
 import com.group8.dalsmartteamwork.database.CallStoredProcedure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StudentEnrollmentDaoImpl implements IStudentEnrollmentDao {
-    @Override
-    public Boolean assignCourseToUser(String userId, int courseId) {
-        CallStoredProcedure procedure = null;
-        try {
-            procedure = new CallStoredProcedure("spEnrollStudentToCourse(?, ?)");
-            procedure.setParameter(1, userId);
-            procedure.setParameter(2, courseId);
-            procedure.execute();
-            return true;
-        } catch (Exception e) {
-            //TODO: Add to Log
-            e.printStackTrace();
-        } finally {
-            if (null != procedure) {
-                procedure.cleanup();
-            }
-        }
-        return false;
-    }
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public Boolean assignCourseToUser(String userId, int courseId) {
+		CallStoredProcedure procedure = null;
+		try {
+			procedure = new CallStoredProcedure("spEnrollStudentToCourse(?, ?)");
+			procedure.setParameter(1, userId);
+			procedure.setParameter(2, courseId);
+			procedure.execute();
+			LOGGER.info(String.format("Course assigned. BannerID: %s, CourseID: %s", userId, courseId));
+			return true;
+		} catch (Exception e) {
+			LOGGER.error("Exception occurred while assigning course to user.", e);
+		} finally {
+			if (null != procedure) {
+				procedure.cleanup();
+			}
+		}
+		LOGGER.warn(String.format("Failed to assign course. BannerID: %s, CourseID: %s", userId, courseId));
+		return false;
+	}
 }
