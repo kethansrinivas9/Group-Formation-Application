01c72fa6691caefd50abb90d55d5e8fc619073e6
ridha <rd428850@dal.ca>
2020-07-16 07:05:33 -0300

    Added Logs to Course Package

--- src/main/java/com/group8/dalsmartteamwork/course/controllers/InstructorController.java
+++ src/main/java/com/group8/dalsmartteamwork/course/controllers/InstructorController.java
@@ -4,6 +4,8 @@ import com.group8.dalsmartteamwork.course.dao.CourseDaoImpl;
 import com.group8.dalsmartteamwork.course.dao.ICourseDao;
 import com.group8.dalsmartteamwork.course.models.CourseInstructorManagerImpl;
 import com.group8.dalsmartteamwork.course.models.ICourseInstructorManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,42 +15,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InstructorController {
-    @GetMapping("/instructor")
-    public String getInstructor() {
-        return "instructor";
-    }
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("/viewcourse/{courseid}")
-    public String view(@PathVariable int courseid, Model model) {
-        ICourseDao courseDao = new CourseDaoImpl();
-        ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
-        if (courseInstructorManager.courseExists(courseid)) {
-            model.addAttribute("course", courseid);
-            model.addAttribute("currentTAList", courseInstructorManager.getCurrentTAs(courseid));
-            model.addAttribute("currentStudentList", courseInstructorManager.getCurrentStudents(courseid));
-            return "instructorCourseHome";
-        }
-        return "badrequest";
-    }
+	@GetMapping("/instructor")
+	public String getInstructor() {
+		return "instructor";
+	}
 
-    @PostMapping("/add-ta")
-    public String addTA(@RequestParam(name = "courseid") int courseid, Model model) {
-        ICourseDao courseDao = new CourseDaoImpl();
-        ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
-        model.addAttribute("users", courseInstructorManager.getEligibleTAs(courseid));
-        model.addAttribute("courseid", courseid);
-        return "addTA";
-    }
+	@GetMapping("/viewcourse/{courseid}")
+	public String view(@PathVariable int courseid, Model model) {
+		ICourseDao courseDao = new CourseDaoImpl();
+		ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
+		if (courseInstructorManager.courseExists(courseid)) {
+			model.addAttribute("course", courseid);
+			model.addAttribute("currentTAList", courseInstructorManager.getCurrentTAs(courseid));
+			model.addAttribute("currentStudentList", courseInstructorManager.getCurrentStudents(courseid));
+			return "instructorCourseHome";
+		} else {
+			LOGGER.warn("Trying to access course that does not exist. CourseID: " + courseid);
+			return "badrequest";
+		}
+	}
 
-    @GetMapping("/confirm-add-ta")
-    public String confirmAddTA(@RequestParam(name = "courseid") int courseID,
-                               @RequestParam(name = "bannerid") String bannerID, Model model) {
-        ICourseDao courseDao = new CourseDaoImpl();
-        ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
-        if (courseInstructorManager.addTAtoCourse(bannerID, courseID)) {
-            model.addAttribute("courseid", courseID);
-            return "successAddTA";
-        }
-        return "badrequest";
-    }
+	@PostMapping("/add-ta")
+	public String addTA(@RequestParam(name = "courseid") int courseid, Model model) {
+		ICourseDao courseDao = new CourseDaoImpl();
+		ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
+		model.addAttribute("users", courseInstructorManager.getEligibleTAs(courseid));
+		model.addAttribute("courseid", courseid);
+		return "addTA";
+	}
+
+	@GetMapping("/confirm-add-ta")
+	public String confirmAddTA(@RequestParam(name = "courseid") int courseID,
+							   @RequestParam(name = "bannerid") String bannerID, Model model) {
+		ICourseDao courseDao = new CourseDaoImpl();
+		ICourseInstructorManager courseInstructorManager = new CourseInstructorManagerImpl(courseDao);
+		if (courseInstructorManager.addTAtoCourse(bannerID, courseID)) {
+			model.addAttribute("courseid", courseID);
+			return "successAddTA";
+		} else {
+			LOGGER.warn(String.format("Trying to add TA failed. CourseID: %s, BannerID: %s", courseID, bannerID));
+			return "badrequest";
+		}
+	}
 }
